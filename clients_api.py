967164f4ccde2c651c8db71b4dfcd8eec2bda664
ne_lox <liviolettassfinger@gmail.com>
2023-08-09 23:43:07 +0400

    dsadas

--- clients_api.py
+++ clients_api.py
@@ -25,12 +25,12 @@ class ClientsAPI:
 
     def __init__(self):
 
-        self.proxy = {
-            "proxy_type": "socks5",
-            "addr": "127.0.0.1",
-            "port": 9050,
-            "rdns": True
-        }
+        # self.proxy = {
+        #     "proxy_type": "socks5",
+        #     "addr": "127.0.0.1",
+        #     "port": 9050,
+        #     "rdns": True
+        # }
 
         self.auth_code_data = {
 
